4bffeb74534fc867fa7af7684bbe19d738d1c3e0
cynjjj <108981687+cynjjj@users.noreply.github.com>
2022-07-10 09:37:55 +0800

    Update main.py

--- yolov5-master/main.py
+++ yolov5-master/main.py
@@ -83,7 +83,7 @@ if __name__ == '__main__':
             with st.spinner(text='资源加载中...'):
                 st.sidebar.image(uploaded_file)
                 picture = Image.open(uploaded_file)
-                picture = picture.save(f'data/images/{uploaded_file.name}')
+                picture = picture.save(f'./data/images/{uploaded_file.name}')
                 opt.source = f'data/images/{uploaded_file.name}'
         else:
             is_valid = False
